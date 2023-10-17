4023ce9c2467d0ebfe84bf8ca7d8dd08905002b7
ArthurLVDH <squishylxix@gmail.com>
2023-10-17 11:45:55 -0700

    file upload working

--- converter.js
+++ converter.js
@@ -1,6 +1,6 @@
-function convert() {
-    console.log("ran");
-    fetch("2.heic")
+function convert(file) {
+    console.log("file converting");
+    fetch(URL.createObjectURL(file))
         .then((res) => res.blob())
         .then((blob) => heic2any({
             blob,
@@ -9,7 +9,7 @@ function convert() {
         }))
         .then((conversionResult) => {
             var url = URL.createObjectURL(conversionResult);
-            document.getElementById("target").innerHTML = `<a target="_blank" href="${url}"><img src="${url}"></a>`;
+            document.getElementById("target").innerHTML += `<a target="_blank" href="${url}"><img src="${url}"></a>`;
         })
         .catch((e) => {
             console.log(e);
@@ -17,5 +17,13 @@ function convert() {
 }
 
 const fileInput = document.getElementById('input');
-function uploadFile() {
+const convertButton = document.getElementById('convertButton');
+function uploadFiles() {
+    console.log("files uploaded")
+    convertButton.style.backgroundColor = "rgb(228, 227, 145)";
+    const files = [...fileInput.files];
+    files.forEach(file => {
+        convert(file);
+    });
+    convertButton.style.backgroundColor = "rgb(145, 228, 155)";
 }
