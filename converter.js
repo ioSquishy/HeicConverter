69feda98223ec9b688d7343a6d112c676087ccc3
ArthurLVDH <squishylxix@gmail.com>
2023-10-19 12:44:45 -0700

    done

--- converter.js
+++ converter.js
@@ -9,21 +9,58 @@ function convert(file) {
         }))
         .then((conversionResult) => {
             var url = URL.createObjectURL(conversionResult);
-            document.getElementById("target").innerHTML += `<a target="_blank" href="${url}"><img src="${url}"></a>`;
+            download(url);
         })
         .catch((e) => {
             console.log(e);
+            window.alert(e.message);
+            download(null);
         });
 }
 
 const fileInput = document.getElementById('input');
 const convertButton = document.getElementById('convertButton');
+const progress = document.getElementById("progress");
+var filesToDownload = 0;
+var filesDownloaded = 0;
+var currentlyDownloading = false;
 function uploadFiles() {
     console.log("files uploaded")
-    convertButton.style.backgroundColor = "rgb(228, 227, 145)";
+    if (currentlyDownloading) {
+        window.alert("Wait until all your currently downloading files have been downloaded!");
+        return;
+    } else {
+        currentlyDownloading = true;
+        
+    }
     const files = [...fileInput.files];
+    filesToDownload = files.length;
+    convertButton.style.backgroundColor = "rgb(228, 227, 145)";
+    let progText = "Files Downloaded: 0/" + filesToDownload;
+    progress.innerText = progText;
     files.forEach(file => {
         convert(file);
     });
-    convertButton.style.backgroundColor = "rgb(145, 228, 155)";
 }
+
+function download(url) {
+    console.log("downloadingg");
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = url.split('/').pop();
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    filesDownloaded++;
+    let progText = "Files Downloaded: " + filesDownloaded + "/" + filesToDownload;
+    progress.innerText = progText;
+    if (filesDownloaded >= filesToDownload) {
+        convertButton.style.backgroundColor = "rgb(145, 228, 155)";
+        filesDownloaded = 0;
+        currentlyDownloading = false;
+    }
+}
+
+fileInput.onchange = () => {
+    convertButton.style.backgroundColor = "rgb(145, 214, 228)";
+};
